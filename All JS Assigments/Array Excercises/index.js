52a2829856572c4229efd403019bb9ff73c44acd
Saeed Ali <saeedaliofficial77@gmail.com>
2025-04-12 21:26:17 +0500

    4th one completed

--- All JS Assigments/Array Excercises/index.js
+++ All JS Assigments/Array Excercises/index.js
@@ -4,33 +4,48 @@
 //       ====push method====
 // ● Add multiple elements to an array using push and return the updated array
 
-let arr = ["Saeed","Ali","Kamil","Jerry"];
-console.log(arr);
+// let arr = ["Saeed","Ali","Kamil","Jerry"];
+// console.log(arr);
 
-arr.push("Khan","Sufiyan","Mushtaque");
+// arr.push("Khan","Sufiyan","Mushtaque");
 
-console.log(arr);
+// console.log(arr);
 
 // ● Create a function that adds numbers from 1 to 10 to an empty array using push.
 
-const addNums = ()=>{
-    let arr = [];
+// const addNums = ()=>{
+//     let arr = [];
 
-    for(let i = 1; i<=10;i++){
-        arr.push(i)
-    }
-    console.log(arr); 
-}
+//     for(let i = 1; i<=10;i++){
+//         arr.push(i)
+//     }
+//     console.log(arr); 
+// }
 
-addNums()
+// addNums()
 
 //● Use push to add a nested array [5, 6] to an existing array.
 
-let arr1 = [3,4];
-let arr2 = [5,6];
-arr1.push(arr2)
-console.log(arr1);
+// let arr1 = [3,4];
 
+// let arr2 = [5,6];
 
+// arr1.push(arr2)
 
+// console.log(arr1);
 
+//result [ 3, 4, [ 5, 6 ] ]
+
+// ● Write a function that uses push to add a string to an array and then logs the new length
+// of the array.
+
+const addStr = () =>{
+    let arrStr = ["Jerry","Saeed","Ali"];
+    //before
+    console.log(arrStr.length);
+
+    arrStr.push("Kamil");
+    //after
+    console.log(arrStr.length);
+}
+addStr()
