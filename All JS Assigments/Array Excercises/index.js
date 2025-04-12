58e4e87d658179c266509b7e54c8371f58d89734
Saeed Ali <saeedaliofficial77@gmail.com>
2025-04-12 21:08:34 +0500

    first question completed

---
+++ All JS Assigments/Array Excercises/index.js
@@ -0,0 +1,14 @@
+
+//      ====Array Exercises====
+
+//       ====push method====
+// ● Add multiple elements to an array using push and return the updated array
+
+let arr = ["Saeed","Ali","Kamil","Jerry"];
+console.log(arr);
+
+arr.push("Khan","Sufiyan","Mushtaque");
+
+console.log(arr);
+
+
